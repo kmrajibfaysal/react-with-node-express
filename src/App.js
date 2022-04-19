048a157ed7cfa99e17c88faecaf6889f14a5c63f
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-20 05:21:07 +0600

    added simple form page to show fetched user and post user

--- src/App.js
+++ src/App.js
@@ -10,12 +10,31 @@ function App() {
     }, []);
 
     return (
-        <div className="m-36">
+        <div className="mx-auto h-full w-full bg-pink-100">
             <h1 className="text-6xl font-black">{users.length} </h1>
-            <ul className="bg-slate-500 p-4 text-center text-2xl">
+            <form className="mx-auto mb-20 w-1/2 text-gray-700">
+                <input
+                    className="my-4 rounded p-2 text-xl"
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                />
+                <input
+                    className="my-4 rounded p-2 text-xl"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                />{' '}
+                <br />
+                <input className="rounded bg-green-400 px-6 py-2" type="submit" value="Add user" />
+            </form>
+            <ul className="bg-slate-500 p-4 text-center text-xl">
                 {users.map((user) => (
-                    <li key={user.id}>
-                        Name:{user.name}, Email: {user.email}
+                    <li
+                        className="my-2 mx-auto w-5/6 cursor-pointer rounded-full bg-slate-200 p-4 transition-all duration-150 hover:scale-x-105 hover:bg-green-400 hover:shadow-lg"
+                        key={user.id}
+                    >
+                        id:{user.id}, Name:{user.name}, Email: {user.email}
                     </li>
                 ))}
             </ul>
