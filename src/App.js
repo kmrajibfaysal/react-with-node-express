aa868bfec34f47386c5501520c53f28230967141
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-20 03:36:26 +0600

    added intial files and some basic set up code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
+
     return (
-        <div>
-            <h1 className="text-9xl">Hello world!</h1>
+        <div className="m-36">
+            <h1 className="text-6xl font-black">{users.length} </h1>
         </div>
     );
 }
